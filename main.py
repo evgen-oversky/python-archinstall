c5008e8560bd424008520158473addd0ff1e385b
oversky <oversky>
2025-10-07 12:22:46 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import sys
+import subprocess
+import time
+
+start_time = time.time()
+
+def run_command(cmd):
+    try:
+        result = subprocess.run(cmd, check=True capture_output=True, text=True)
+        return result
+    except subprocess.CalledProcessError as e:
+        print(f"{e}")
+        print(f"{e.stderr}")
+        sys.exit(1)
+
+def check_internet():
+    pass
+
+
+
+
+print(run_command(["ping", "-c", "1", "8.8.8.8"]).returncode)
+
+
+
+
+
+
+
+
+duration = time.time() - start_time
+print(f"{duration:.2f}")
+
